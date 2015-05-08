0d545f45bcb1ba7ceb1150ac6595127a25b1c37d
Tony Narlock <tony@git-pull.com>
2015-05-08 14:28:53 -0500

    compat: try to get Python 3 to reraise with new type

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/_compat.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/_compat.py
@@ -76,8 +76,8 @@ else:
 
     def reraise(tp, value, tb=None):
         if value.__traceback__ is not tb:
-            raise(value.with_traceback(tb))
-        raise value
+            raise tp(value.with_traceback(tb))
+        raise tp(value)
 
 
 number_types = integer_types + (float,)
